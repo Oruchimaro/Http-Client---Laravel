bc8cca13adb00f959852534e47c765dd11e31903
Amir Daneshkar <amir.daneshkar@hotmail.com>
2020-01-29 08:17:29 +0330

    Adding links to protected actions for authenticated users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,7 +37,16 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        <li class="nav-item"><a href="{{ route('products.publish') }}" class="btn btn-success">publish</a></li>
+                        @auth
+                        <li class="nav-item">
+                            <a href="{{ route('purchases') }}" class="nav-link"> Purchases </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('products') }}" class="nav-link"> Products </a>
+                        </li>
 
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
